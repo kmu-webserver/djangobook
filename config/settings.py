84c90646f7159c53c83fb366f2385dfdeefdb6ea
suyeon <suyeon1104@gmail.com>
2024-06-10 10:38:07 +0900

    allauth 에서 출력하는 로그인 성공 메시지 제거

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 from pathlib import Path
 
+from django.contrib import messages
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -28,6 +30,7 @@ ALLOWED_HOSTS = ["pybo.embition.ai", "*"]
 CSRF_TRUSTED_ORIGINS = ["http://pybo.embition.ai", "https://pybo.embition.ai"]
 ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'http'
 SOCIALACCOUNT_LOGIN_ON_GET = True
+MESSAGE_LEVEL = messages.constants.WARNING
 
 # Application definition
 
