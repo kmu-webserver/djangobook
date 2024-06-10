9830f2ab02b010cc96cec6aba78d4c745a9e9e58
meoowwing <eojin28@naver.com>
2024-06-10 10:30:18 +0900

    카테고리 모델 추가

--- pybo/models.py
+++ pybo/models.py
@@ -2,6 +2,13 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Question(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
     subject = models.CharField(max_length=200)
@@ -9,6 +16,7 @@ class Question(models.Model):
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
     voter = models.ManyToManyField(User, related_name='voter_question')
+    category = models.ForeignKey('Category', null=False, blank=False, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.subject
